481ae885d3c13bf1fe9c8656faec2238f34cf6c2
Andrew Carlson <acarl005@g.ucla.edu>
2015-10-28 12:37:42 -0700

    simplify authtoken service

--- frontend/app/scripts/services/authtoken.js
+++ frontend/app/scripts/services/authtoken.js
@@ -2,24 +2,19 @@
 
 angular.module('tokenAuthApp')
   .factory('authToken', function ($window) {
-    var storage = $window.localStorage,
-        cachedToken;
+    var storage = $window.localStorage;
 
     var authToken = {
       setToken: function(token) {
-        cachedToken = token;
         storage.setItem('userToken', token);
       },
       getToken: function() {
-        if (!cachedToken)
-          cachedToken = storage.getItem('userToken');
-        return cachedToken;
+        return storage.getItem('userToken');
       },
       isAuthenticated: function() {
         return !!authToken.getToken();
       },
       removeToken: function() {
-        cachedToken = null;
         storage.removeItem('userToken');
       }
     };
